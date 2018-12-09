62e19d6595b2d882510d6f2ae37c98af99baa326
mohkan1 <43953894+mohkan1@users.noreply.github.com>
2018-12-09 12:51:23 +0100

    just test 3

---
+++ test/action.php
@@ -0,0 +1,20 @@
+<?php
+  include_once 'http://mohkan1.github.io/test/testphp.php';
+?>
+
+
+
+<?php
+
+if(isset($_POST['open'])){
+  $sql = "UPDATE openclosedoor SET action='1' WHERE command='command1'";
+  mysqli_query($conn, $sql);
+}
+
+
+if(isset($_POST['close'])){
+  $sql = "UPDATE openclosedoor SET action='2' WHERE command='command1'";
+  mysqli_query($conn, $sql);
+}
+
+?>
