3d8119874a319c1eaa98e8e1d3b8910595b24f8a
mohkan1 <43953894+mohkan1@users.noreply.github.com>
2018-12-10 10:55:37 +0100

    test 6

--- test/action.php
+++ test/action.php
@@ -1,18 +1,17 @@
 <?php
   include_once 'mohkan1.github.io/test/testphp.php';
-?>
 
 
 
-<?php
 
-if(isset($_POST['open'])){
+$open = filter_input(INPUT_POST, "open", FILTER_SANITIZE_NUMBER_INT);
+
+if($open == 1) {
   $sql = "UPDATE openclosedoor SET action='1' WHERE command='command1'";
   mysqli_query($conn, $sql);
 }
 
-
-if(isset($_POST['close'])){
+if($open == 2) {
   $sql = "UPDATE openclosedoor SET action='2' WHERE command='command1'";
   mysqli_query($conn, $sql);
 }
